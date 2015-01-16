32d18978758b9bb42812bd8b270740cb8466633a
Ozan Çağlayan <ozancag@gmail.com>
2015-01-16 22:43:04 +0200

    Compute a smaller grid to mimic the gameboard

--- digitize.py
+++ digitize.py
@@ -29,22 +29,27 @@ class Dots(object):
     def __init__(self, points):
         self.dot_list = [Dot(p) for p in points]
 
+        # np array, shape <nb_of_points> x 2
+        self.mat = np.array([(d.x, d.y) for d in self.dot_list])
+
+        # This rounds off glitches of point coordinates on the same axis
+        self.rmat = self.mat / 10
+        self.cols = np.unique(self.rmat[:, 0]).size
+        self.rows = np.unique(self.rmat[:, 1]).size
+
     def dump(self):
         for dot in self.dot_list:
             print "(%4d, %4d) (Color: %s, ID: %2d)" % (dot.x, dot.y,
             COLORS[dot.hexcolor], dot.color_id)
 
-    def normalize(self):
-        pass
-
 output = subprocess.Popen(["./contours", sys.argv[1]], stdout=subprocess.PIPE).communicate()[0]
 dots = Dots(eval(output))
 
 dots.dump()
-dots.normalize()
 
 grid = np.zeros((WIDTH, HEIGHT), dtype=np.uint8)
 for dot in dots.dot_list:
     grid[dot.x, dot.y] = dot.color_id
 
 print "%d dots. " % np.sum(grid > 0)
+print "Ideal grid size: %d x %d" % (dots.rows, dots.cols)
